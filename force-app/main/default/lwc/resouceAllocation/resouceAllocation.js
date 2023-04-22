4f5da1674bfab2301cb0efe526371f5695878a30
johangio92 <johan_revilla_92@hotmail.com>
2023-04-22 10:39:43 -0300

    add css to view the data

--- force-app/main/default/lwc/resouceAllocation/resouceAllocation.js
+++ force-app/main/default/lwc/resouceAllocation/resouceAllocation.js
@@ -1,14 +1,15 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
 import getResources from '@salesforce/apex/ControllerResourceAllocation.getResources';
 import getProjectItems from '@salesforce/apex/ControllerResourceAllocation.getProjectItems'
 
 export default class ResouceAllocation extends LightningElement {
+	
+	@api
+	recordId;
+	resources;
+	projectItems;
 
-	projectId;
-	resources
-	projectItems
-
-	@wire(getResources, { projectId: 'a00Dp000001lIIXIA2' })
+	@wire(getResources, { projectId: '$recordId' })
 	_getResources({data, error}) {
 		if (data) {
 			console.log('has data getResources: ' + data);
@@ -21,7 +22,7 @@ export default class ResouceAllocation extends LightningElement {
 		}
 	}
 	
-	@wire(getProjectItems, { projectId: 'a00Dp000001lIIXIA2' })
+	@wire(getProjectItems, { projectId: '$recordId' })
 	_getProjectItems({data, error}) {
 		if (data) {
 			console.log('has data getProjectItems: ' + data);
