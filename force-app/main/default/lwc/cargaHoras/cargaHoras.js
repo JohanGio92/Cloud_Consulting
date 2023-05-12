0feed6a680975d749f19c183f908e87d53f84e2e
johangio92 <johan_revilla_92@hotmail.com>
2023-05-11 21:14:00 -0300

    finish project

--- force-app/main/default/lwc/cargaHoras/cargaHoras.js
+++ force-app/main/default/lwc/cargaHoras/cargaHoras.js
@@ -1,116 +1,133 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire} from 'lwc';
 import getResourceProjects from '@salesforce/apex/AsignedTaskController.getResourceProjects';
+import {updateRecord} from 'lightning/uiRecordApi';
 
-//const columns = [
-//    { label: 'Project Name', fieldName: 'name' },
-//    { label: 'Task Name', fieldName: 'taskName' },
-//    { label: 'State', fieldName: 'state' },
-//    { label: 'Estimated Hours', fieldName: 'EstimatedHours', type: 'number' },
-//    { label: 'Registered Hours', fieldName: 'RegisteredHours', type: 'number' },
-//];
-
-//const projects = [
-//    {
-//        name: 'Proyecto Rompe bytes',
-//        tareas: [
-//            {
-//                taskName: 'Crear Modelo de Datos',
-//                state: 'In Progress',
-//                EstimatedHours: 34,
-//                RegisteredHours: 12,
-//                init:true,
-//                id:1
-//            },
-//            {
-//                taskName: 'Armado de reportes',
-//                state: 'No init',
-//                EstimatedHours: 25,
-//                RegisteredHours: 0,
-//                init:false,
-//                id:2
-//            },
-//            {
-//                taskName: 'Configurar Perfiles',
-//                state: 'No init',
-//                EstimatedHours: 12,
-//                RegisteredHours: 0,
-//                init:false,
-//                id:3
-//            }
-//        ]
-//    },
-//    {
-//        name: 'Proyecto Innovar',
-//        tareas: [
-//            {
-//                taskName: 'Crear Validation Rules',
-//                state: 'In Progress',
-//                EstimatedHours: 20,
-//                RegisteredHours: 8,
-//                init:true,
-//                id:4
-//            },
-//            {
-//                taskName: 'Apex Trigger Position',
-//                state: 'No init',
-//                EstimatedHours: 40,
-//                RegisteredHours: 0,
-//                init:false,
-//                id:5
-//            }
-//        ]
-//    },
-//];
+import STATUS_FIELD from '@salesforce/schema/Assigment_Task__c.Status__c';
+import REPORTEDHOURS_FIELD from '@salesforce/schema/Assigment_Task__c.ReportedHours__c';
 
 export default class CargaHoras extends LightningElement {
-    //columns = columns;
-	resourceProjects;
+
+    // Creamos una variable/propiedad llamada resourceProjects.
+
+    resourceProjects;
+
+    // Usamos un wire para conectarnos a un metodo de APEX, para poder traernos la data (resourceProjects).
 
 	@wire(getResourceProjects)
 	_getResourceProjects({data, error}) {
 		if (data) {
 			this.resourceProjects = data;
-			console.log("data:" + data);
+			console.log("data: " + data);
 		} else {
-			console.log("error:" + error);
+			console.log("error: " + error);
 		}
 	}
 
-    registeredHours;
+    // Almacenar el ID de la task INICIADA.
+
+    selectedTaskId;
 
     // Metodo que al hacer click sobre el boton Iniciar captura el id de la task.
-    handleClickInit(event){
-        const taskId = event.target.dataset.id;
-        this.searchTaskById(taskId)
+    handleClickStart(event){
+        this.selectedTaskId = event.target.dataset.id;
+        this.handleUpdate();
     }
 
-    searchTaskById(id) {
-        this.resourceProjects.forEach(resourceProject => {
-          resourceProject.Assigment_Tasks__r.forEach( task => {
-            if(task.id == id){
-                console.log(JSON.stringify(task));
-            }
-          })
-        });
+    // Metodo que actualiza el estado de la tarea de "Not Init" a "In Progress".
 
-        //projects.forEach(project => {
-        //  project.tareas.forEach(task => {
-        //    if(task.id == id){
-        //        console.log(JSON.stringify(task));
-        //    }
-        //  })
-        //});
+    handleUpdate() {
+        const fields = {};
+        fields[STATUS_FIELD.fieldApiName] = 'In Progress';
+        
+        // Este recordInput sería el objeto (Registro) que actualiza la DB.
+        const recordInput = { fields };
+
+        recordInput.recordId = this.selectedTaskId;
+
+        updateRecord(recordInput)
+            .then(() => {
+            	// pruebo con location.reload()
+                window.location.reload()
+            })
+            .catch(error => {
+                console.log(error)
+            });
     }
-      
 
-    //Metodo que me trae el registro/objeto task que pido por ID.
-    // --------------------------------------------------------------------------------------
-  
-
-    //--- handleChange se encarga de capturar el valor de las horas registradas, para almacenarlas en una propiedad/variable de la clase. ---
+    registeredHours;
 
     handleChange(event){
         this.registeredHours = event.target.value;
-        console.log(this.registeredHours)
+    }
+
+    // Metodo que toma las horas capturadas por el metodo handleChange ( metodo que toma valor (value) de input al escribir)
+    // este metodo invoca al metodo handleAddRegisteredHoursUpdate pasandole como argumentos el id de la tarea y las horas reportadas
+    // hasta el momento.
+
+    handleClickRegisterHours(event){
+        let hoursUpToNow = event.target.dataset.reportedhours;
+        const taskId = event.target.dataset.id;
+
+        if(hoursUpToNow === undefined){
+            hoursUpToNow = 0;
+        }
+
+        this.handleAddRegisteredHoursUpdate(hoursUpToNow, taskId);
+    }
+
+    // Este metodo se encarga de hacer la suma de las horas registras hasta el momento, más las horas que le queremos agregar
+    // Generando una suma, esa suma se agrega en el status (campo/field) y agregamos el ID de la tarea para especificarle al updateRecord
+    // que registro debe actualizar (hacer un update en DB).
+
+    handleAddRegisteredHoursUpdate(reportedHoursNow, identifier){
+        const fields = {};
+        const sum = Number(reportedHoursNow) + Number(this.registeredHours);
+        
+        fields[REPORTEDHOURS_FIELD.fieldApiName] = sum;
+        
+        // Este recordInput sería el objeto que le dice a la DB que registro(identifier) debe actualizar y qué campos 
+        //modificar(fields).
+
+        const recordInput = { fields };
+
+        recordInput.recordId = identifier;
+
+        updateRecord(recordInput)
+            .then(() => {
+                window.location.reload()
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }
+
+    // Metodo que toma los valores necesarios para hacer la actualizacion del status de la task a "Completed"
+    
+    handleClickCompleted(event){
+        const status = event.target.dataset.status;
+        const taskId = event.target.dataset.id;
+        if(status !== 'Completed'){
+            this.handleStatusCompletedUpdate(taskId);
+        }   
+    }
+
+    //Este metodo hace el update a "Completed" del campo Status__c.
+
+    handleStatusCompletedUpdate(taskId) {
+        const fields = {};
+        fields[STATUS_FIELD.fieldApiName] = 'Completed';
+        
+        // Este recordInput sería el objeto (Registro) que actualiza la DB.
+        const recordInput = { fields };
+
+        recordInput.recordId = taskId;
+
+        updateRecord(recordInput)
+            .then(() => {
+                window.location.reload()
+            })
+            .catch(error => {
+                console.log(error)
+            });
     }
 }
